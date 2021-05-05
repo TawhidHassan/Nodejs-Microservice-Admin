c59e62df457424a8494df5a3332b4162b93a43e9
sifat hassan <sifat007.sh7@gmail.com>
2021-05-05 13:12:25 +0600

    install and setup

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import * as express from 'express'
+import {Request, Response} from 'express'
+import * as cors from 'cors'
+
+
+const app = express()
+
+app.use(cors({
+    origin: ['http://localhost:3000', 'http://localhost:8080', 'http://localhost:4200']
+}))
+
+app.use(express.json())
+
+console.log('Listening to port: 8000')
+app.listen(8000)
